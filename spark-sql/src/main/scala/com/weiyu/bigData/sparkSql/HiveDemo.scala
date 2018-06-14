8ad88243134d36bd63b1343288d3b5241731903c
weiyu <weiyu@gomeholdings.com>
2018-06-14 17:38:51 +0800

    规范代码

---
+++ spark-sql/src/main/scala/com/weiyu/bigData/sparkSql/HiveDemo.scala
@@ -0,0 +1,12 @@
+package com.weiyu.bigData.sparkSql
+
+import org.apache.spark.sql.SparkSession
+
+object HiveDemo {
+
+  def main(args: Array[String]): Unit = {
+    val session = SparkSession.builder().master("local").appName("hiveDemo").config("spark.sql.warehouse.dir","spark-warehouse").enableHiveSupport().getOrCreate()
+    val dbs = session.sql("show databases")
+    dbs.show
+  }
+}
