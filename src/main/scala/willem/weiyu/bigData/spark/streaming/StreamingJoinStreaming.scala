18f18135f8a440a3b3d7aff0aef2e664e293b2c7
weiyu <weiyu005@ke.com>
2018-11-07 18:36:34 +0800

    添加流关联示例代码

--- src/main/scala/willem/weiyu/bigData/spark/streaming/StreamingJoinStreaming.scala
+++ src/main/scala/willem/weiyu/bigData/spark/streaming/StreamingJoinStreaming.scala
@@ -9,14 +9,14 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
   * @Date 2018/11/05 18:20
   */
 object StreamingJoinStreaming {
-//  val HOST = "localhost"
-  val HOST = "10.26.27.81"
+  val HOST = "localhost"
+//  val HOST = "10.26.27.81"
   val PORT = 9999
 
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "D:\\hadoop-2.8.5")
     val conf = new SparkConf().setMaster("local[4]").setAppName("streamingDemo")
-    val ssc = new StreamingContext(conf,Seconds(5))
+    val ssc = new StreamingContext(conf,Seconds(2))
     val lines = ssc.socketTextStream(HOST, PORT)
 
     /**
