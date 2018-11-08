498023934377826f127e3780fcce66aa1fb95427
weiyu <weiyu005@ke.com>
2018-11-08 20:25:05 +0800

    添加流关联示例代码

--- src/main/scala/willem/weiyu/bigData/spark/streaming/StreamingDemo.scala
+++ src/main/scala/willem/weiyu/bigData/spark/streaming/StreamingDemo.scala
@@ -9,12 +9,16 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
   * @Date 2018/11/05 18:20
   */
 object StreamingDemo {
+  val CHECKPOINT_PATH = "/spark/checkpoint"
+  val DURATION = 5
+  //  val HOST = "localhost"
+  val HOST = "10.26.27.81"
 
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "D:\\hadoop-2.8.5")
     val conf = new SparkConf().setMaster("local[4]").setAppName("streamingDemo")
     val ssc = new StreamingContext(conf,Seconds(5))
-    val lines = ssc.socketTextStream("localhost", 9999)
+    val lines = ssc.socketTextStream(HOST, 9999)
 
     /**
       * 按空格分隔
