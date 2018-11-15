a8e96d11b121eb1bd96d72b24b6d01cc257578df
weiyu <weiyu005@ke.com>
2018-11-15 18:46:04 +0800

    将发送至kafka的数据改为json形式

--- src/main/java/simulator/KafkaProducerSimulator.java
+++ src/main/java/simulator/KafkaProducerSimulator.java
@@ -1,10 +1,12 @@
 package simulator;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -15,6 +17,7 @@ import java.util.concurrent.ExecutionException;
 public class KafkaProducerSimulator {
     public static final String TOPIC = "test";
     public static final String BOOTSTRAP_SERVER = "10.26.27.81:9092";
+    public static final String[] OS_TYPE = new String[]{"Android", "IOS", "Other", "None"};
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // create instance for properties to access producer configs
@@ -46,13 +49,19 @@ public class KafkaProducerSimulator {
 
         KafkaProducer kafkaProducer = new KafkaProducer<String, String>(props);
 
-        Runtime.getRuntime().addShutdownHook(new Thread(()-> kafkaProducer.close()));
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaProducer::close));
 
         while (true){
             Random rand = new Random();
-            ProducerRecord record = new ProducerRecord<String,String>(TOPIC,"message_"+rand.nextInt(100));
-            kafkaProducer.send(record);
-            Thread.sleep(1500);
+            JSONObject event = new JSONObject();
+            event.put("uid", UUID.randomUUID());
+            event.put("event_time", System.currentTimeMillis());
+            event.put("os_type",OS_TYPE[rand.nextInt(4)]);
+            event.put("click_count",rand.nextInt(10));
+            String message = event.toString();
+            System.out.println("******产生消息:"+message);
+            kafkaProducer.send(new ProducerRecord<String,String>(TOPIC,message));
+            Thread.sleep(2000);
         }
     }
 }
